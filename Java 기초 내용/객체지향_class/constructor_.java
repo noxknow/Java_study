c34cde801ad3ecdf783f7b1080a88b00cafbe60e
Chiwang Lee <122594223+noxknow@users.noreply.github.com>
2023-02-03 21:08:26 +0900

    Update constructor_.java

--- Java 기초 내용/객체지향_class/constructor_.java
+++ Java 기초 내용/객체지향_class/constructor_.java
@@ -1,37 +1,37 @@
-// package Practice;
+package Practice;
 
-// class Person {
+class Person {
     
-//     // 속성
-//     int age;
-//     String name;
+    // 속성
+    int age;
+    String name;
 
-//     // 생성자
-//     Person() {} // 받는 인자값이 없는 경우
-//     Person(int age, String name){  // 받는 인자값이 두개인 경우
-//         this.age = age;
-//         this.name = name;
-//     }
+    // 생성자
+    Person() {} // 받는 인자값이 없는 경우
+    Person(int age, String name){  // 받는 인자값이 두개인 경우
+        this.age = age;
+        this.name = name;
+    }
     
-//     // 메서드
-//     void printperson(){
-//         System.out.println("나이 : " + age + ", 이름 : " + name);
-//     }
-// }
+    // 메서드
+    void printperson(){
+        System.out.println("나이 : " + age + ", 이름 : " + name);
+    }
+}
 
-// public class constructor_ {
-//     public static void main(String[] args) {
-//         Person p1 = new Person(20, "홍길동");
+public class constructor_ {
+    public static void main(String[] args) {
+        Person p1 = new Person(20, "홍길동");
 
-//         System.out.println(p1); // 주소
-//         System.out.println(p1.age); // 20
-//         System.out.println(p1.name); // 홍길동
-//         p1.printperson(); // 객체 p1이 호출 하는 것
+        System.out.println(p1); // 주소
+        System.out.println(p1.age); // 20
+        System.out.println(p1.name); // 홍길동
+        p1.printperson(); // 객체 p1이 호출 하는 것
 
-//         Person p2 = new Person(30, "이순신");
-//         p2.printperson(); // 객체 p2가 호출 하는 것
-//     }
-// }
+        Person p2 = new Person(30, "이순신");
+        p2.printperson(); // 객체 p2가 호출 하는 것
+    }
+}
 
 /*
 make에서와 다른 점은 make는 public class안에 함수를 만든거고, 이 경우는 밖에 새로운
