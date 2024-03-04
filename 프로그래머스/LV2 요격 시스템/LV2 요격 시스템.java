f6304f0a9ab42703a07625e6d1207ea97520aec5
Chiwang Lee <122594223+noxknow@users.noreply.github.com>
2024-03-04 16:28:19 +0900

    Create LV2 요격 시스템.java

---
+++ 프로그래머스/LV2 요격 시스템/LV2 요격 시스템.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        
+        Arrays.sort(targets, (o1, o2) -> {
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            
+            return o1[1] - o2[1];
+        });
+        
+        int before = 0;
+        for(int[] target : targets){
+            if(before <= target[0]){
+                before = target[1];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
